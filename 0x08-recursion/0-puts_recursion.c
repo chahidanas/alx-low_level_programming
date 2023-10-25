873836995247b3d2caf110cc8c1fa6d1de5da049
pinehub98 <anaschahid5@outlook.com>
2023-10-25 17:21:09 +0100

    0.2

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,15 @@
 
 /**
   * _puts_recursion - main function
-  *
-  * @s: function parameter
-  *
-  * Return: Always 0.
+  * @s: parameter of the fucntion
   */
+
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	_putchar('\n');
