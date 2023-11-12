7f28153e5923a188a2adf346def2b00decda2823
chahidanas <chahidanas27@gmail.com>
2023-11-12 17:27:15 +0100

    101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -57,21 +57,21 @@ char **strtow(char *str)
 		if (str[i] == ' ' || str[i] == '\0')
 		{
 			if (c)
-				{
-					end = i;
-					tmp = (char *)malloc(sizeof(char) * (c + 1));
+			{
+				end = i;
+				tmp = (char *)malloc(sizeof(char) * (c + 1));
 
-					if (tmp == NULL)
-						return (NULL);
+				if (tmp == NULL)
+					return (NULL);
 
-					while (start < end)
-						*tmp++ = str[start++];
+				while (start < end)
+					*tmp++ = str[start++];
 
-					*tmp = '\0';
-					matrix[k] = tmp - c;
-					k++;
-					c = 0;
-				}
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
 		}
 		else if (c++ == 0)
 			start = i;
