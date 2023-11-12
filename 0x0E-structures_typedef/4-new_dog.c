809bf952dc59eed72df27d3a7db078035bb2afda
chahidanas <chahidanas27@gmail.com>
2023-11-13 00:41:36 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -79,6 +79,5 @@ dog_t *new_dog(char *name, float age, char *owner){
 	}
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
-	dog->age = age;	
-	return (dog);
+	dog->age = age;return (dog);
 }
